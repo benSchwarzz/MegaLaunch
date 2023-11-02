a0d39b47719947c70c0780a377f7e68200fb4b1d
Ben Schwarz <147880835+benSchwarzz@users.noreply.github.com>
2023-11-02 19:40:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,55 @@ class User:
         self.sprite1 = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (6).png")
         self.sprite2 = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (7).png")
         self.current_sprite = self.sprite2
-        self.hit_box = pg.Rect(self.x, self.y, self.width, self.height)
 
-    def draw(self):
+    def draw(self, left, right):
+        self.hit_box = pg.Rect(self.x, self.y, self.width, self.height)
         screen.blit(self.current_sprite, self.hit_box)
+        keys = pg.key.get_pressed()
+        powered = False
+
+        ### y direction
+        if not powered:
+            self.dy += 0.5
+            
+            if self.y < 300:
+                self.y = 300
+
+            if self.dy < 0:
+                if self.y > 300:
+                    self.y += self.dy
+            elif self.dy > 0:
+                if self.y > HEIGHT-self.height:
+                    self.y += self.dy
+            self.y += self.dy
+
+        ###
+
+        ### x direction
+        if keys[left]:
+            if self.dx > -10:
+                self.dx -= 0.5
+            self.x += self.dx
+        
+        if keys[right]:
+            if self.dx < 10:
+                self.dx += 0.5
+            self.x += self.dx
+        
+        if not keys[left] and not keys[right]:
+            if self.dx < 0:
+                self.dx += 0.5
+            if self.dx > 0:
+                self.dx -= 0.5
+            self.x += self.dx
+
+        if self.x < -60:
+            self.x = WIDTH
+        elif self.x > WIDTH:
+            self.x = -60
+        ###
+            
+
     
 class Cloud:
     def __init__(self, y, width, height, dy):
@@ -48,25 +93,55 @@ class Cloud:
             self.y = -60
             self.x = random.randint(-40, WIDTH-20)
 
+class Fuel:
+    def __init__(self, x, dy):
+        self.width = 25
+        self.height = 41
+        self.x = x
+        self.y = -42
+        self.dy = dy
+        self.image = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (9).png")
+        self.sprite = pg.transform.scale(self.image, (25, 41))
+    
+    def draw(self):
+        self.hit_box = pg.Rect(self.x, self.y, self.width, self.height)
+        screen.blit(self.sprite, self.hit_box)
+
+        self.y += self.dy
+
+
 
 def main():
-    player = User(220, 300, 60, 100, 0, 0)
-    cloud1 = Cloud(-100, 100, 60, 3)
-    cloud2 = Cloud(-150, 150, 90, 3)
-    cloud3 = Cloud(-50, 90, 50, 3)
+    player = User(220, 300, 60, 100, -10, 0)
+    last_time: int = pg.time.get_ticks()
+
+    fuels = []
 
     run = True
     while run:
         screen.fill(SKY_BLUE)
 
-        cloud1.move()
-        cloud1.draw()
-        cloud2.move()
-        cloud2.draw()
-        cloud3.move()
-        cloud3.draw()
+        new_time: int = pg.time.get_ticks()
+        if (new_time - last_time) > random.randint(1000, 4000):
+            last_time = pg.time.get_ticks()
 
-        player.draw()
+            print("created")
+
+            fuel = Fuel(random.randint(0, WIDTH-25), -20)
+            
+            fuels.append(fuel)
+
+        for f in fuels:
+            if player.dy < 0:
+                f.dy = -1*player.dy
+            else:
+                f.dy = 0
+            
+            if f.y > HEIGHT+2:
+                fuels.remove(f)
+            f.draw()
+
+        player.draw(pg.K_a, pg.K_d)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
