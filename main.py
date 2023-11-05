b3816b21176b4ca0f26b9c8053838003d089b654
Ben Schwarz <147880835+benSchwarzz@users.noreply.github.com>
2023-11-05 09:12:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,35 +12,39 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 
 class User:
-    def __init__(self, x, y, width, height, dy, dx):
+    def __init__(self, x, y, width, height, dy, dx, gravity):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.dy = dy
         self.dx = dx
+        self.gravity = gravity
         self.sprite1 = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (6).png")
         self.sprite2 = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (7).png")
         self.current_sprite = self.sprite2
+        self.powered = False
 
     def draw(self, left, right):
         self.hit_box = pg.Rect(self.x, self.y, self.width, self.height)
+
+        if self.powered == True:
+            self.current_sprite = self.sprite1
+        else: 
+            self.current_sprite = self.sprite2
+        
         screen.blit(self.current_sprite, self.hit_box)
         keys = pg.key.get_pressed()
-        powered = False
 
         ### y direction
-        if not powered:
-            self.dy += 0.2
+        self.dy += self.gravity
             
-            if self.dy < 0:
-                if self.y > 400:
-                    self.y += self.dy
-
-            elif self.dy > 0:
+        if self.dy < 0:
+            if self.y > 400:
                 self.y += self.dy
-        
 
+        elif self.dy > 0:
+            self.y += self.dy
         ###
 
         ### x direction
@@ -70,11 +74,11 @@ class User:
 
     
 class Cloud:
-    def __init__(self, y, width, height, dy):
-        self.x = random.randint(-40, WIDTH -20)
-        self.y = y
-        self.width = width
-        self.height = height
+    def __init__(self, x, dy):
+        self.x = x
+        self.y = -50
+        self.width = 100
+        self.height = 50
         self.dy = dy
         self.sprite = pg.image.load("SpaceShip Adventure/images/pixil-frame-0 (8).png")
         
@@ -82,13 +86,10 @@ class Cloud:
     def draw(self):
         self.box = pg.Rect(self.x, self.y, self.width, self.height)
         screen.blit(self.sprite, self.box)
-    def move(self):
+
         self.y += self.dy
 
-        if self.y > HEIGHT+40:
-            pg.time.wait(random.randint(50, 200))
-            self.y = -60
-            self.x = random.randint(-40, WIDTH-20)
+        
 
 class Fuel:
     def __init__(self, x, dy):
@@ -109,22 +110,56 @@ class Fuel:
 
 
 def main():
-    player = User(220, 300, 60, 100, -10, 0)
+    player = User(220, 300, 60, 100, -10, 0, 0.1)
     last_time: int = pg.time.get_ticks()
+    last_time1: int = pg.time.get_ticks()
+    sprite_time: int = 0
+    initial_time = pg.time.get_ticks()
+    elapsed_time: int = 0
+    fuel_range: list = [400, 1000]
+
 
     fuels = []
+    clouds = []
 
     run = True
     while run:
+        elapsed_time = pg.time.get_ticks() - initial_time
         screen.fill(SKY_BLUE)
 
+        if player.y > HEIGHT + 20:
+            run = False
+
+        if elapsed_time > 10000:
+            pass
+
+        if initial_time < 60000:
+            player.gravity = 0.05
+
+        new_time1: int = pg.time.get_ticks()
+        if (new_time1 - last_time1) > random.randint(400, 1200):
+            last_time1 = pg.time.get_ticks()
+            
+            cloud = Cloud(random.randint(0, WIDTH-25), -20)
+            clouds.append(cloud)
+
+        for c in clouds:
+            if player.dy < 0:
+                c.dy = -1*player.dy
+            else:
+                c.dy = 0
+            
+            if c.y > HEIGHT+2:
+                clouds.remove(c)
+            c.draw()
+
         new_time: int = pg.time.get_ticks()
-        if (new_time - last_time) > random.randint(1000, 4000):
+        if (new_time - last_time) > random.randint(fuel_range[0], fuel_range[1]):
             last_time = pg.time.get_ticks()
             
             fuel = Fuel(random.randint(0, WIDTH-25), -20)
             fuels.append(fuel)
-
+        
         for f in fuels:
             if player.dy < 0:
                 f.dy = -1*player.dy
@@ -134,6 +169,20 @@ def main():
             if f.y > HEIGHT+2:
                 fuels.remove(f)
             f.draw()
+
+        try:
+            for f in fuels:
+                if f.hit_box.colliderect(player.hit_box):
+                    fuels.remove(f)
+                    sprite_time = elapsed_time
+                    player.powered = True
+                    player.dy -= 6
+        except:
+            pass
+
+        if (elapsed_time - sprite_time) > 1000:
+            player.powered = False
+
 
         player.draw(pg.K_a, pg.K_d)
 
