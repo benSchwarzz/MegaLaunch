388db8df61c86317ba7bccdc04fb6541fea3878c
Ben Schwarz <brbs615@gmail.com>
2023-11-07 16:43:53 -0500

    New Powerup feature

--- Code/main.py
+++ Code/main.py
@@ -4,12 +4,15 @@ import pygame.freetype
 from Classes.user import User
 from Classes.fuels import Fuel
 from Classes.clouds import Cloud
+from Classes.powerups import Speed_boost
+
 
 pg.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-SKY_BLUE = (135, 206, 235)
+init_R, init_G, init_B = 120, 200, 255
+SKY_BLUE = init_R, init_G, init_B
 font = pygame.freetype.Font(None, 20)
 
 WIDTH, HEIGHT = 500, 700
@@ -19,17 +22,18 @@ clock = pg.time.Clock()
 
 def main():
     player = User(220, 300, 60, 100, -10, 0, 0.07)
-    last_time: int = pg.time.get_ticks()
-    last_time1: int = pg.time.get_ticks()
     sprite_time: int = 0
+    last_time = pg.time.get_ticks()
+    last_time1 = pg.time.get_ticks()
+    last_time2 = pg.time.get_ticks()
     initial_time = pg.time.get_ticks()
     elapsed_time: int = 0
     fuel_range: list = [400, 1000]
     altitude = 0
 
-
     fuels = []
     clouds = []
+    powerups = []
 
     run = True
     while run:
@@ -91,6 +95,36 @@ def main():
             player.powered = False
 
 
+
+
+
+
+
+        new_time2: int = pg.time.get_ticks()
+        if (new_time2 - last_time2) > random.randint(10000, 20000):
+            last_time2 = pg.time.get_ticks()
+            
+            powerup = Speed_boost(random.randint(0, WIDTH-25), -20)
+            powerups.append(powerup)
+
+        for p in powerups:
+            if player.dy < 0:
+                p.dy = -1*player.dy
+            else:
+                p.dy = 0
+            
+            if p.y > HEIGHT+2:
+                powerups.remove(p)
+            p.draw(screen)
+
+        for p in powerups:
+            if p.hit_box.colliderect(player.hit_box):
+                player.dy -= 20
+                player.powered = True
+                sprite_time = elapsed_time
+                powerups.remove(p)
+
+
         player.draw(screen, pg.K_a, pg.K_d)
 
         altitude += int((-1*player.dy*elapsed_time)/1000)
@@ -107,3 +141,4 @@ def main():
 
 
 main()
+
